1fd9bc26a3979c1aa664413beefc31e953be95cf
Joel Abrahamsson <mail@joelabrahamsson.com>
2013-09-01 11:56:10 +0200

    Added GUID to page types

--- MVC/FruitCorp.Web/Models/Pages/StandardPage.cs
+++ MVC/FruitCorp.Web/Models/Pages/StandardPage.cs
@@ -3,7 +3,8 @@ using EPiServer.DataAnnotations;
 
 namespace FruitCorp.Web.Models.Pages
 {
-  [ContentType]
+  [ContentType(
+    GUID = "1cfe38ca-025e-4f66-a237-226c44f586e9")]
   public class StandardPage : PageData
   {
     public virtual string MainIntro { get; set; }
--- MVC/FruitCorp.Web/Models/Pages/StartPage.cs
+++ MVC/FruitCorp.Web/Models/Pages/StartPage.cs
@@ -3,7 +3,8 @@ using EPiServer.DataAnnotations;
 
 namespace FruitCorp.Web.Models.Pages
 {
-  [ContentType]
+  [ContentType(
+    GUID = "3e06c3bd-4bdf-4bbd-b745-07df69567502")]
   public class StartPage : PageData
   {
   }
