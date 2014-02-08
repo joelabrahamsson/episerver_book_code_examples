07b46a6c29f294732fdee9f82afab92343a17601
Joel Abrahamsson <mail@joelabrahamsson.com>
2014-02-08 21:02:59 +0100

    Meta description validator class

---
+++ MVC/FruitCorp.Web/Business/MetaDescriptionValidator.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Framework.Localization;
+using EPiServer.Validation;
+using FruitCorp.Web.Models.Pages;
+using System.Collections.Generic;
+
+namespace FruitCorp.Web.Business
+{
+  public class MetaDescriptionValidator : IValidate<StandardPage>
+  {
+    public IEnumerable<ValidationError> Validate(StandardPage page)
+    {
+      if (string.IsNullOrWhiteSpace(page.MetaDescription))
+      {
+        yield return new ValidationError()
+          {
+            ErrorMessage = LocalizationService.Current.GetString("/validation/metadescription")
+          };
+      }
+    }
+  }
+}
