fb2a34d168777acf2b196c6287013aa1d494b245
Joel Abrahamsson <mail@joelabrahamsson.com>
2014-07-28 16:19:20 +0200

    Synced changes made to MVC project into Web Forms project

--- WebForms/FruitCorp.Web/Models/Pages/BasePage.cs
+++ WebForms/FruitCorp.Web/Models/Pages/BasePage.cs
@@ -1,11 +1,24 @@
 ﻿using EPiServer.Core;
+using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace FruitCorp.Web.Models.Pages
 {
   public abstract class BasePage : PageData
   {
-    public virtual string Title { get; set; }
+    [Display(GroupName = "Meta data", Order = 10)]
+    public virtual string Title
+    {
+      get { return this.GetPropertyValue(x => x.Title); }
+      set { this.SetPropertyValue(x => x.Title, value); }
+    }
 
-    public virtual string MetaDescription { get; set; }
+    [Display(GroupName = "Meta data", Order = 20)]
+    [UIHint(UIHint.Textarea)]
+    public virtual string MetaDescription
+    {
+      get { return this.GetPropertyValue(x => x.MetaDescription); }
+      set { this.SetPropertyValue(x => x.MetaDescription, value); }
+    }
   }
 }
--- WebForms/FruitCorp.Web/Models/Pages/StandardPage.cs
+++ WebForms/FruitCorp.Web/Models/Pages/StandardPage.cs
@@ -1,5 +1,7 @@
 ﻿using EPiServer.Core;
 using EPiServer.DataAnnotations;
+using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace FruitCorp.Web.Models.Pages
 {
@@ -10,9 +12,52 @@ namespace FruitCorp.Web.Models.Pages
   [AvailablePageTypes(Include = new[] { typeof(StandardPage) })]
   public class StandardPage : BasePage
   {
+    [Display(GroupName = "Meta data", Order = 10)]
+    public override string Title
+    {
+      get
+      {
+        var title = base.Title;
+
+        if (string.IsNullOrEmpty(title))
+        {
+          title = PageName;
+        }
+
+        return title;
+      }
+      set
+      {
+        base.Title = value;
+      }
+    }
+
+    [Display(GroupName = "Meta data", Order = 20)]
+    [UIHint(UIHint.Textarea)]
+    public override string MetaDescription
+    {
+      get
+      {
+        var metaDescription = base.MetaDescription;
+
+        if (string.IsNullOrEmpty(metaDescription))
+        {
+          metaDescription = MainIntro;
+        }
+
+        return metaDescription;
+      }
+      set
+      {
+        base.MetaDescription = value;
+      }
+    }
+
+    [Display(Order = 10)]
+    [UIHint(UIHint.Textarea)]
     public virtual string MainIntro { get; set; }
 
+    [Display(Order = 20)]
     public virtual XhtmlString MainBody { get; set; }
-
   }
 }
