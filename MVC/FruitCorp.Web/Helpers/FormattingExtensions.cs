f2a8b95bbe799821937adfa18efbfc3716584e87
Joel Abrahamsson <mail@joelabrahamsson.com>
2014-08-05 16:33:39 +0200

    LineBreaked and Image display templates

---
+++ MVC/FruitCorp.Web/Helpers/FormattingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+namespace FruitCorp.Web.Helpers
+{
+  public static class FormattingExtensions
+  {
+    public static HtmlString ToHtmlLineBreaks(this string value)
+    {
+      var parsed = string.Empty;
+      if (!string.IsNullOrEmpty(value))
+      {
+        parsed = HttpUtility.HtmlEncode(value);
+        parsed = parsed.Replace("\n", "<br />");
+      }
+
+      return new HtmlString(parsed);
+    }
+  }
+}
