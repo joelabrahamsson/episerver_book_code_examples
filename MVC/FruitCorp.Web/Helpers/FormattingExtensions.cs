a2c5298601d054301e02e2b4e2c2feac992f10e6
Joel Abrahamsson <mail@joelabrahamsson.com>
2014-08-06 20:19:32 +0200

    Custom XhtmlString and MainIntro rendering

--- MVC/FruitCorp.Web/Helpers/FormattingExtensions.cs
+++ MVC/FruitCorp.Web/Helpers/FormattingExtensions.cs
@@ -1,4 +1,9 @@
 ﻿using System.Web;
+using System.Web.Mvc;
+using CsQuery;
+using EPiServer.Core;
+using EPiServer.Framework.Localization;
+using EPiServer.Web.Mvc.Html;
 
 namespace FruitCorp.Web.Helpers
 {
@@ -14,6 +19,29 @@ namespace FruitCorp.Web.Helpers
       }
 
       return new HtmlString(parsed);
+    }
+
+    public static IHtmlString CustomXhtmlString(this HtmlHelper helper, XhtmlString value)
+    {
+      var htmlString = helper.XhtmlString(value).ToHtmlString();
+      
+      var headline = LocalizationService.Current
+        .GetString("/views/common/linklist/headline");
+      
+      var cq = new CQ(htmlString, HtmlParsingMode.Document);
+      cq.Find("ul.link-list").Each(list =>
+        {
+          var box = CQ.CreateFragment(
+            "<div class=\"link-list\">" 
+            + "<h4><span class=\"glyphicon glyphicon-link\"></span> " 
+            + headline
+            + "</h4></div>");
+          list.RemoveClass("link-list");
+          box.Append(list.OuterHTML);
+          list.Cq().ReplaceWith(box);
+        });
+
+      return new MvcHtmlString(cq.Find("body").Html());
     }
   }
 }
--- WebForms/FruitCorp.Web/Views/Pages/Standard.aspx
+++ WebForms/FruitCorp.Web/Views/Pages/Standard.aspx
@@ -1,7 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/MasterPages/WithSubNavigation.master" AutoEventWireup="true" CodeBehind="Standard.aspx.cs" Inherits="FruitCorp.Web.Views.Pages.Standard" %>
 
 <asp:Content ContentPlaceHolderID="MainContent" runat="server">
+    <EPiServer:Property PropertyName="Image" Width="100%" runat="server" />
     <EPiServer:Property PropertyName="PageName" CustomTagName="h1" runat="server" />
     <EPiServer:Property PropertyName="MainIntro" CustomTagName="p" CssClass="lead" runat="server" />
-    <EPiServer:Property PropertyName="MainBody" />
+    <EPiServer:Property PropertyName="MainBody" runat="server" />
 </asp:Content>
